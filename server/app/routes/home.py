b0933cca0f2c37a1f56e1f50eb1cefd697c9960d
Thang Tran <drakiez92@gmail.com>
2021-09-17 21:21:23 +0700

    add create post

--- server/app/routes/home.py
+++ server/app/routes/home.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required
 
 home_routes = Blueprint('home', __name__)
 
 @home_routes.route('/')
+@jwt_required()
 def index():
     return jsonify('ok'), 201
--- server/app/routes/post.py
+++ server/app/routes/post.py
@@ -1,7 +1,47 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models import User, Post, db
+from slugify import slugify
+
+from datetime import datetime
 
 post_routes = Blueprint('post', __name__)
 
-@post_routes.route('/')
+@post_routes.route('/create', methods=['POST'])
+@jwt_required()
 def index():
-    return jsonify('ok'), 201
+    userEmail = get_jwt_identity()
+    body = request.json
+    user = User.query.filter_by(email=userEmail).first()
+
+    now = datetime.now().isoformat()
+
+    ## create slug
+    
+    slug = slugify(user.slug + body['title'], max_length=50, to_lower=True)
+
+    ## get summary
+    summary = ''
+    contentSplit = body['content'].split(' ')
+    if len(contentSplit) > 40:
+        summary = ' '.join(contentSplit[:40])
+    else:
+        summary = ' '.join(contentSplit)
+
+    post = Post(
+        authorId=user.id,
+        title=body['title'],
+        subtitle=body['subtitle'],
+        slug=slug,
+        summary=summary,
+        published=0 if body['isDraft'] is True else 1,
+        createdAt=now,
+        bannerLink=body['banner'],
+        publishedAt=None if body['isDraft'] else now,
+        content=body['content']
+    )
+
+    db.session.add(post)
+    db.session.commit()
+
+    return jsonify({'test': post.slug}), 201
